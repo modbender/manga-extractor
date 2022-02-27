427b28436bc69906c82532eeb600737fff648b5c
modbender <rameshmamathayashas@gmail.com>
2022-02-27 17:24:27 +0530

    init mangadex provider

---
+++ README.md
@@ -0,0 +1,11 @@
+# Manga Extractor
+
+A simple manga extractor. Extracts any comic info, chapter list and chapter pages. This is an app that strictly provides API.
+
+**Still in development.**
+
+## Installations
+
+Not yet online but will soon be, and then it will be like below
+
+`pip install manga-extractor`
---
+++ manga_extractor/__init__.py
@@ -0,0 +1,38 @@
+from urllib.parse import urlparse
+from typing import List, Dict, Union, Type
+
+from manga_extractor import providers
+
+
+DATACALL = {
+    'manga': 'get_manga',
+    'chapter': 'get_chapter',
+    'chapters': 'get_manga_chapters',
+    'cover': 'get_cover',
+}
+
+
+class MangaExtractor:
+
+    def __init__(self, type_list: list, url: str):
+        self.url = url
+        self.type_list = type_list
+
+        self.parsed_url = urlparse(url)
+        self.provider_classes = self.all_providers()
+
+    def all_providers(self):
+        return providers.__all__
+
+    def get(self):
+        data = {}
+        for ProviderSite in self.provider_classes:
+            provider_instance = ProviderSite(url=self.url)
+            if self.parsed_url.netloc == provider_instance.domain:
+                for data_type in self.type_list:
+                    data[data_type] = getattr(provider_instance, DATACALL[data_type])()
+
+
+        print(data['manga'].title)
+
+        return data
