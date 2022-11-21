cfe31544af7a8ab069c0413e2e333623dd1e7164
modbender <rameshmamathayashas@gmail.com>
2022-11-21 15:28:32 +0530

    v1.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ README = open('README.md').read()
 
 setuptools.setup(
     name='mext',
-    version='1.1.1',
+    version='1.1.2',
     url='https://github.com/modbender/manga-extractor',
     description='A simple manga extractor. Extracts any comic info, chapter list and chapter pages. In development.',
     long_description=README,
